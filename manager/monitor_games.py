d1addbab390645683fc39f64e3db37ac16670488
Wrycu <blah380@gmail.com>
2016-08-14 14:52:05 -0400

    feat(game): add game monitor loop

---
+++ manager/monitor_games.py
@@ -0,0 +1,29 @@
+import asyncio
+
+
+def monitor_games(loop):
+    """
+    Monitor games
+    :param loop:
+    :return:
+    """
+    # TODO:
+    #  1. Actually monitor games
+    #  2. Configure an actual try / except block which logs failures (possibly even sending them to Discord)
+    try:
+        print("hello")
+        loop.call_later(1, monitor_games, loop)
+    except Exception:
+        loop.stop()
+
+
+def create_game_loop():
+    """
+    Start the event loop which will actually monitor games for their status and perform actions
+    The Discord bot will run on the same event loop
+    :return:
+        An event loop which can be used to run other functions on the same loop
+    """
+    loop = asyncio.get_event_loop()
+    loop.call_later(30, monitor_games, loop)
+    return loop
