bbf7fdadb74eceff488e6abcacb6d44412940eb3
Wrycu <blah380@gmail.com>
2016-08-14 14:52:46 -0400

    feat(discord): add loading from config

--- config.py
+++ config.py
@@ -1,11 +1,48 @@
-from jsonschema import validate, ValidationError
+from jsonschema import validate
 from json import load
-from config_schema import configSchema
+import logging
+import discord
+from config_schema import config_schema
+from manager.monitor_discord import DiscordBot
+from manager.monitor_games import create_game_loop
 
-def loadConfig(filename = "config.example.json"):
-    with open(filename) as data_file:
-        data = load(data_file)
-    data_file.close()
-    validate(data, configSchema)
-    print(validate(data, configSchema))
-    return data
+
+class Config:
+    def __init__(self, config_file="config.json"):
+        self._configure_logger()
+        with open(config_file) as data_file:
+            data = load(data_file)
+        data_file.close()
+        validate(data, config_schema)
+        self.discord_user = data['discord']['username']
+        self.discord_token = data['discord']['oauth_token']
+        self.channels = data['discord']['channels']
+        self.admins = {
+            'global': data['discord']['global_admins'],
+        }
+        self.games = []
+        for attrs in data['games']:
+            self.admins[attrs['name']] = attrs['admins']
+            # TODO: Create a game object here
+            if attrs['name'] not in self.games:
+                self.games.append(attrs)
+        self.required_roles = data['discord']['required_roles']
+        self.games = data['games']
+        self.game_loop = create_game_loop()
+        self.discord_client = discord.Client()
+        bot = DiscordBot(self.logger, self.channels, self.admins, self.required_roles, self.games)
+        bot.register_events(self.discord_client)
+        self.discord_client.run(self.discord_token, loop=self.game_loop)
+
+    def _configure_logger(self):
+        """
+        Creates and configures a logger to log to "game_manager.log"
+        :return:
+        """
+        self.logger = logging.getLogger("DiscordGameManager")
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler('game_manager.log')
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
--- manager/monitor_discord.py
+++ manager/monitor_discord.py
@@ -1,116 +1,136 @@
-import discord
 import asyncio
 
-client = discord.Client()
 
-
-@client.event
-async def on_ready():
-    print('Logged in as', client.user.name)
-
-
-@client.event
-async def on_message(message):
+class DiscordBot:
     """
-    The bot has received a message
-    :param message:
-        Message object passed by the discord client
-    :return:
-        N/A
+    Create an object which allows you to register events on it for interacting with Discord while still having
+        features such as logging and shared information available to the object
     """
-    # TODO: Read this from the config object
-    channels = [
-        'testeroni'
-    ]
-    roles = [
-        'member',
-    ]
-    commands = [
-        '!help',
-        '!start',
-        '!stop'
-    ]
+    def __init__(self, logger, channels, admins, required_roles, games):
+        """
+        Initialize the object
+        :param logger:
+            A logger object for logging (duh).  It should already be configured to log wherever you want
+        :param channels:
+            A list of channels in which to listen for messages
+        :param admins:
+            A dict of people allowed to execute the more privileged commands.
+            Note that "global" admins are allowed to do anything
+            Additionally, "global" is always present (though it may be empty) while any other grouping is dependent
+                on the game being loaded
+            {
+                'global':
+                    [ ],
+                'tf2':
+                    [ ],
+            }
+        :param required_roles:
+            A list of roles required for the bot to respond to users.  Useful for making it so non-registered
+                users cannot do silly things
+        :param games:
+            A list of games. Basically a straight read from the config file at the moment, though this is expected
+                to evolve into a list of "game" objects which can have commands executed on them
+        """
+        self.logger = logger
+        self.channels = channels
+        self.admins = admins
+        self.roles = required_roles
+        self.games = games
 
-    full_local_user = str(client.user.name) + '#' + str(client.user.discriminator)
+    def register_events(self, client):
+        """
+        The Discord library runs async, so we need to register functions to be called on events.
+        :param client:
+            An already configured and logged in Discord client
+        :return:
+        """
+        @client.event
+        async def on_ready():
+            """
+            We have successfully logged in (and are online)
+            :return:
+            """
+            self.logger.warn("Logged in")
+            print('Logged in as', client.user.name)
 
-    # Do they have the appropriate roles?
-    has_role = False
-    for role in message.author.roles:
-        if str(role) in roles:
-            has_role = True
-            break
+        @client.event
+        async def on_message(message):
+            """
+            Parse messages for commands and pass the handling off
+            :param message:
+                Message object passed by the discord client
+            :return:
+                N/A
+            """
+            commands = [
+                '!help',
+                '!start',
+                '!stop'
+            ]
 
-    if str(message.author) != full_local_user and str(message.channel) in channels and has_role:
-        for command in commands:
-            if str(message.content).startswith(command):
-                await client.send_typing(message.channel)
-                await handle_command(
-                    command,
-                    int(message.author.id),
-                    str(message.content).replace(command, ''),
-                    message.channel
-                )
+            full_local_user = str(client.user.name) + '#' + str(client.user.discriminator)
 
+            # Do they have the appropriate roles?
+            has_role = False
+            for role in message.author.roles:
+                if str(role) in self.roles:
+                    has_role = True
+                    break
 
-async def handle_command(command, user, game, channel):
-    """
-    Parse a chat command and invoke the appropriate function for it.
-    :param command:
-        String. Name of the command being executed.
-    :param user:
-        Integer. ID of the user executing a command.
-    :param game:
-        String. Name of the game being impacted (can be an empty string for certain commands)
-    :param channel:
-        Discord channel object. Used to send messages to.
-    :return:
-    """
-    # Remove leading space
-    if game and game[0] == ' ':
-        game = game[1:]
-    # TODO: Read from config object
-    # Global admins can do anything to anything
-    global_admins = [
-        108005836579696640,  # Tim
-    ]
+            if has_role and str(message.author) != full_local_user and str(message.channel) in self.channels:
+                for command in commands:
+                    if str(message.content).startswith(command):
+                        await client.send_typing(message.channel)
+                        await handle_command(
+                            command,
+                            int(message.author.id),
+                            str(message.content).replace(command, ''),
+                            message.channel
+                        )
 
-    # Game admins can only change their own games
-    game_admins = {
-        'tf2': [
-            108337116919910400,  # Jeremy
-        ],
-        'empyrion': [],
-    }
+        async def handle_command(command, user, game, channel):
+            """
+            Parse a chat command and invoke the appropriate function for it.
+            :param command:
+                String. Name of the command being executed.
+            :param user:
+                Integer. ID of the user executing a command.
+            :param game:
+                String. Name of the game being impacted (can be an empty string for certain commands)
+            :param channel:
+                Discord channel object. Used to send messages to.
+            :return:
+            """
+            # Remove leading space if it exists
+            if game and game[0] == ' ':
+                game = game[1:]
 
-    if not game and command != '!help':
-        await client.send_message(channel, 'You must include a game.')
-    elif command == '!help':
-        await client.send_message(channel, 'Available commands: !start, !stop, !status.')
-    elif command == '!start':
-        if game in game_admins.keys():
-            # TODO: Actually start the game
-            await asyncio.sleep(5)
-            await client.send_message(channel, 'Server for ' + game + ' started!')
-        else:
-            await client.send_message(channel, game + ' not supported!')
-    elif command == '!stop':
-        if user not in global_admins and user not in game_admins[game]:
-            await client.send_message(channel, 'You do not have permission to stop this game.')
-        elif game in game_admins.keys():
-            # TODO: Actually stop the game
-            await asyncio.sleep(5)
-            await client.send_message(channel, 'Server for ' + game + ' stopped!')
-        else:
-            await client.send_message(channel, game + ' not supported!')
-    elif command == '!status':
-        if game in game_admins.keys():
-            # TODO: Actually get the status of the game
-            await asyncio.sleep(5)
-            await client.send_message(channel, game + ' has two players!')
-        else:
-            await client.send_message(channel, game + ' not supported!')
-    else:
-        await client.send_message(channel, 'Unknown command.')
-
-# TODO: move to config file. When Kevin is done with that part
-client.run('KEY_FROM_CONFIG_FILE')
+            if not game and command != '!help':
+                await client.send_message(channel, 'You must include a game.')
+            elif command == '!help':
+                await client.send_message(channel, 'Available commands: !start, !stop, !status.')
+            elif command == '!start':
+                if game in self.games:
+                    # TODO: Actually start the game
+                    await asyncio.sleep(5)
+                    await client.send_message(channel, 'Server for ' + game + ' started!')
+                else:
+                    await client.send_message(channel, game + ' not supported!')
+            elif command == '!stop':
+                if user not in self.admins['global'] and user not in self.admins[game]:
+                    await client.send_message(channel, 'You do not have permission to stop this game.')
+                elif game in self.games:
+                    # TODO: Actually stop the game
+                    await asyncio.sleep(5)
+                    await client.send_message(channel, 'Server for ' + game + ' stopped!')
+                else:
+                    await client.send_message(channel, game + ' not supported!')
+            elif command == '!status':
+                if game in self.games:
+                    # TODO: Actually get the status of the game
+                    await asyncio.sleep(5)
+                    await client.send_message(channel, game + ' has two players!')
+                else:
+                    await client.send_message(channel, game + ' not supported!')
+            else:
+                await client.send_message(channel, 'Unknown command.')
